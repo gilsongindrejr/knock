f8f8125e88f2349481c88a7208ceaba0aff69854
GilsonGindrejr <gindrejunior@hotmail.com>
2021-08-02 20:45:49 -0300

    changes in imports

--- knock.py
+++ knock.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
-"""knock.py: Um simples port scan, ainda sem scan de host e treading"""
+"""knock.py: A simple portscan, still without host scan and threading"""
 
 __author__ = "GilsonJr"
 __version__ = "1.0"
-__status__ = "Desenvolvimento"
+__status__ = "Development"
 
-from socket import *
-import pyfiglet
+from socket import socket, AF_INET, SOCK_STREAM, SOCK_DGRAM
+from pyfiglet import figlet_format
 
-banner = pyfiglet.figlet_format('Knock!', font='5lineoblique')
+banner = figlet_format('Knock!', font='5lineoblique')
 print(banner)
 
 ip = input('IP: ')
-range = input('Portas (porta-porta): ')
+range = input('port range (port-port): ')
 initial_port = int(range.split('-')[0])
 final_port = int(range.split('-')[1])
 while 1:
@@ -24,12 +24,12 @@ while 1:
         s = socket(AF_INET, SOCK_DGRAM)
         break
     else:
-        print('Entrada inválida')
+        print('Invalid entry')
         continue
 while initial_port != final_port + 1:
     if s.connect_ex((ip, initial_port)) == 0:
         s.close()
-        print(f'Porta {initial_port} aberta')
+        print(f'Port {initial_port} open')
     else:
         pass
     initial_port += 1
