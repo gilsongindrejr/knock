674c7f86a14b72dbd4e635062c2bf0ba89629272
Gilson <gindrejunior@hotmail.com>
2021-12-04 21:55:06 -0300

    small code structure changes

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# knock
-Simple portscan using python socket module
+# Knock
+Simple portscan using python socket module.
+
+# Usage example
+```commandline
+python knock.py -ip 127.0.0.1 -p 0-1000 --proto TCP
+```
+
+# Getting help
+Show basic usage.
+```commandline
+python knock.py
+```
+Show help.
+```commandline
+python knock.py --help
+```
+
+Keep it simple!
--- knock.py
+++ knock.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""knock.py: A simple portscan, still without host scan and threading"""
+"""knock.py: A simple portscan"""
 
 __author__ = "GilsonJr"
-__version__ = "2.2"
+__version__ = "2.3"
 __status__ = "Development"
 
 from socket import socket, AF_INET, SOCK_STREAM, SOCK_DGRAM
-import pyfiglet
+from pyfiglet import figlet_format
 import argparse
 from re import search
 from colorama import Fore
 
-banner = pyfiglet.figlet_format('Knock!', font='5lineoblique')
-
+banner = figlet_format('Knock!', font='5lineoblique')
 
 parser = argparse.ArgumentParser(
     prog='Knock',
@@ -33,28 +32,43 @@ def print_banner():
 
 
 def port_knocker(ip, ports, proto):
-    initial_port = int(ports.split('-')[0])
-    final_port = int(ports.split('-')[1])
+    # Get ports number
+    try:
+        initial_port = int(ports.split('-')[0])
+        final_port = int(ports.split('-')[1])
+    except Exception:
+        print(Fore.RED + 'Invalid port')
+        return 0
+
+    # Check if ports and proto is valid
+    if initial_port > final_port:
+        print(Fore.RED + 'Initial port must be lower than final port')
+        return 0
+    else:
+        if proto == 'TCP':
+            s = socket(AF_INET, SOCK_STREAM)
+        elif proto == 'UDP':
+            s = socket(AF_INET, SOCK_DGRAM)
+        else:
+            print(Fore.RED + 'Invalid protocol')
+            return 0
+
+    # Regex the ip and try the connection with informed ip address
     try:
         ip_addr = search('[0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}', ip).string
-        if initial_port > final_port:
-            print(Fore.RED + 'Initial port must be lower than final port')
-        else:
-            if proto == 'TCP':
-                s = socket(AF_INET, SOCK_STREAM)
-            elif proto == 'UDP':
-                s = socket(AF_INET, SOCK_DGRAM)
-            else:
-                print(Fore.RED + 'Invalid protocol')
-            while initial_port != final_port + 1:
-                if s.connect_ex((ip_addr, initial_port)) == 0:
-                    s.close()
-                    print(f'Port {initial_port} open')
-                else:
-                    pass
-                initial_port += 1
-    except AttributeError:
+        s.connect_ex((ip_addr, initial_port)) == 0
+    except Exception:
         print(Fore.RED + 'Invalid IP address')
+        return 0
+
+    # loop knocking all doors
+    while initial_port != final_port + 1:
+        if s.connect_ex((ip_addr, initial_port)) == 0:
+            s.close()
+            print(f'Port {initial_port} open')
+        else:
+            pass
+        initial_port += 1
 
 
 if __name__ == '__main__':
